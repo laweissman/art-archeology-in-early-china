a1af7042af12ab43ae8f8353302e64462e3d1527
Lucas Weissman <56417756+laweissman@users.noreply.github.com>
2023-12-05 00:37:40 -0500

    Create scriptThree.js

---
+++ scriptThree.js
@@ -0,0 +1,172 @@
+data = [
+  {
+    period: "Late Neolithic period",
+    startYear: "c. 5000 BCE",
+    endYear: "2000 BCE",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Xia dynasty",
+    startYear: "c. 2000 BCE",
+    endYear: "c. 1600 BCE",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Shang dynasty",
+    startYear: "c. 1500 BCE",
+    endYear: "c. 1050 BCE",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Zhou dynasty",
+    startYear: "c. 1050 BCE",
+    endYear: "256 BCE",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Qin dynasty",
+    startYear: "221 BCE",
+    endYear: "206 BCE",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Han dynasty",
+    startYear: "202 BCE",
+    endYear: "220 CE",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Period of Division",
+    startYear: "220",
+    endYear: "581",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Sui dynasty",
+    startYear: "581",
+    endYear: "617",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Tang dynasty",
+    startYear: "618",
+    endYear: "907",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Five (Northern) dynasties",
+    startYear: "907",
+    endYear: "960",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Ten Kingdoms",
+    startYear: "902",
+    endYear: "979",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Song dynasty",
+    startYear: "960",
+    endYear: "1279",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Yuan dynasty (Mongol)",
+    startYear: "1279",
+    endYear: "1368",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Ming dynasty",
+    startYear: "1368",
+    endYear: "1644",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Qing dynasty (Manchu)",
+    startYear: "1644",
+    endYear: "1911",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "Republic of China",
+    startYear: "1912",
+    endYear: "1949",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+  {
+    period: "People’s Republic of China",
+    startYear: "1949",
+    endYear: "present",
+    notableEvents: [
+      // Placeholder for notable events
+    ],
+    // TODO: Add the correct link
+    link: ""
+  },
+]
