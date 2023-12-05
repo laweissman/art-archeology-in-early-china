59e9cbe1dc9501f735311211f57ee13738c49cea
Lucas Weissman <56417756+laweissman@users.noreply.github.com>
2023-12-04 22:16:52 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Load the JSON data
+  d3.json('flare.json').then(function(data) {
+    // Your D3 tree chart code using the loaded data
+
+    const width = 928;
+    const root = d3.hierarchy(data);
+    const dx = 10;
+    const dy = width / (root.height + 1);
+    const tree = d3.tree().nodeSize([dx, dy]);
+    root.sort((a, b) => d3.ascending(a.data.name, b.data.name));
+    tree(root);
+
+    let x0 = Infinity;
+    let x1 = -x0;
+    root.each(d => {
+      if (d.x > x1) x1 = d.x;
+      if (d.x < x0) x0 = d.x;
+    });
+
+    const height = x1 - x0 + dx * 2;
+
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-dy / 3, x0 - dx, width, height])
+        .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+
+    const link = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5)
+      .selectAll("path")
+        .data(root.links())
+        .join("path")
+          .attr("d", d3.linkHorizontal()
+              .x(d => d.y)
+              .y(d => d.x));
+
+    const node = svg.append("g")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 3)
+      .selectAll("g")
+      .data(root.descendants())
+      .join("g")
+        .attr("transform", d => `translate(${d.y},${d.x})`);
+
+    node.append("circle")
+        .attr("fill", d => d.children ? "#555" : "#999")
+        .attr("r", 2.5);
+
+    node.append("text")
+        .attr("dy", "0.31em")
+        .attr("x", d => d.children ? -6 : 6)
+        .attr("text-anchor", d => d.children ? "end" : "start")
+        .text(d => d.data.name)
+      .clone(true).lower()
+        .attr("stroke", "white");
+
+    // Append the SVG to your chart container
+    document.getElementById('chart').appendChild(svg.node());
+  });
+});
