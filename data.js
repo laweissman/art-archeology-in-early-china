2ffe88eeab90691a404ae697333519ef48d7dd67
Lucas Weissman <56417756+laweissman@users.noreply.github.com>
2023-12-05 00:53:00 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,121 @@
+const data = [
+  {
+    period: "Late Neolithic period",
+    startYear: -5000,
+    endYear: -2000,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Xia dynasty",
+    startYear: -2000,
+    endYear: -1600,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Shang dynasty",
+    startYear: -1500,
+    endYear: -1050,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Zhou dynasty",
+    startYear: -1050,
+    endYear: -256,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Qin dynasty",
+    startYear: -221,
+    endYear: -206,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Han dynasty",
+    startYear: -202,
+    endYear: 220,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Period of Division",
+    startYear: 220,
+    endYear: 581,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Sui dynasty",
+    startYear: 581,
+    endYear: 617,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Tang dynasty",
+    startYear: 618,
+    endYear: 907,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Five (Northern) dynasties",
+    startYear: 907,
+    endYear: 960,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Ten Kingdoms",
+    startYear: 902,
+    endYear: 979,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Song dynasty",
+    startYear: 960,
+    endYear: 1279,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Yuan dynasty (Mongol)",
+    startYear: 1279,
+    endYear: 1368,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Ming dynasty",
+    startYear: 1368,
+    endYear: 1644,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Qing dynasty (Manchu)",
+    startYear: 1644,
+    endYear: 1911,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "Republic of China",
+    startYear: 1912,
+    endYear: 1949,
+    notableEvents: [],
+    link: ""
+  },
+  {
+    period: "People’s Republic of China",
+    startYear: 1949,
+    endYear: new Date().getFullYear(), // Using current year for 'present'
+    notableEvents: [],
+    link: ""
+  },
+];
