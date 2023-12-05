f53add51147d89004bbc86bd1d9476849207ca3f
Lucas Weissman <56417756+laweissman@users.noreply.github.com>
2023-12-04 23:01:05 -0500

    Initial Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Load the JSON data from the flare.json file
+  // Load the JSON data
   d3.json('flare.json').then(function(data) {
+    // Your D3 tree chart code using the loaded data
 
-    // Set the dimensions for the tree layout
     const width = 928;
+    const root = d3.hierarchy(data);
     const dx = 10;
     const dy = width / (root.height + 1);
-    const root = d3.hierarchy(data);
-
-    // Create the tree layout
     const tree = d3.tree().nodeSize([dx, dy]);
     root.sort((a, b) => d3.ascending(a.data.name, b.data.name));
     tree(root);
 
-    // Compute the extent of the tree
     let x0 = Infinity;
     let x1 = -x0;
     root.each(d => {
@@ -21,53 +18,47 @@ document.addEventListener('DOMContentLoaded', function() {
       if (d.x < x0) x0 = d.x;
     });
 
-    // Compute the adjusted height of the tree
     const height = x1 - x0 + dx * 2;
 
-    // Create the SVG element
     const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [-dy / 3, x0 - dx, width, height])
-      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-dy / 3, x0 - dx, width, height])
+        .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
 
-    // Add links (lines) to the SVG
     const link = svg.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5)
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5)
       .selectAll("path")
-      .data(root.links())
-      .join("path")
-        .attr("d", d3.linkHorizontal()
-            .x(d => d.y)
-            .y(d => d.x));
+        .data(root.links())
+        .join("path")
+          .attr("d", d3.linkHorizontal()
+              .x(d => d.y)
+              .y(d => d.x));
 
-    // Add nodes to the SVG
     const node = svg.append("g")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-width", 3)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 3)
       .selectAll("g")
       .data(root.descendants())
       .join("g")
         .attr("transform", d => `translate(${d.y},${d.x})`);
 
-    // Add circles for each node
     node.append("circle")
-      .attr("fill", d => d.children ? "#555" : "#999")
-      .attr("r", 2.5);
+        .attr("fill", d => d.children ? "#555" : "#999")
+        .attr("r", 2.5);
 
-    // Add text for each node
     node.append("text")
-      .attr("dy", "0.31em")
-      .attr("x", d => d.children ? -6 : 6)
-      .attr("text-anchor", d => d.children ? "end" : "start")
-      .text(d => d.data.name)
+        .attr("dy", "0.31em")
+        .attr("x", d => d.children ? -6 : 6)
+        .attr("text-anchor", d => d.children ? "end" : "start")
+        .text(d => d.data.name)
       .clone(true).lower()
         .attr("stroke", "white");
 
-    // Append the SVG element to the chart container
+    // Append the SVG to your chart container
     document.getElementById('chart').appendChild(svg.node());
   });
 });
